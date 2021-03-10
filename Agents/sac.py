a190f6cdf17b764b6f2cb31bb2957c5015bdde82
MahamRashid <mahamrashid74@yahoo.com>
2021-03-10 19:48:26 +0500

    SAC to debug

--- Agents/sac.py
+++ Agents/sac.py
@@ -75,7 +75,7 @@ class SAC(modelFreeAgent.ModelFreeAgent):
         self.critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
         self.actor_optimizer = tf.keras.optimizers.Adam(actor_lr)
 
-        self.actor_network = self.actorModel(empty_state)
+        self.actor_network = self.actorModel()
 
         self.softq_network = self.q_network()
         self.softq_target_network = self.q_network()
@@ -103,8 +103,25 @@ class SAC(modelFreeAgent.ModelFreeAgent):
         self.allMask = np.full((1, self.action_size), 1)
         self.allBatchMask = np.full((self.batch_size, self.action_size), 1)
 
+    def callqFunc(self):
+        # Building up 2 soft q-function with their relative targets
+        input1 = tf.keras.Input(shape=self.state_size, dtype=tf.float64)
+        input2 = tf.keras.Input(shape=self.action_size, dtype=tf.float64)
+        return self.softq_target_network([input1, input2])
 
-    def actorModel(self, state):
+    def callqFunc2(self):
+        # Building up 2 soft q-function with their relative targets
+        input1 = tf.keras.Input(shape=self.state_size, dtype=tf.float64)
+        input2 = tf.keras.Input(shape=self.action_size, dtype=tf.float64)
+        return self.softq_network([input1, input2])
+
+    def callqFunc3(self):
+        # Building up 2 soft q-function with their relative targets
+        input1 = tf.keras.Input(shape=self.state_size, dtype=tf.float64)
+        input2 = tf.keras.Input(shape=self.action_size, dtype=tf.float64)
+        return self.softq_network2([input1, input2])
+
+    def actorModel(self):
         w_bound = 3e-3
 
         input_shape = self.state_size
@@ -121,7 +138,27 @@ class SAC(modelFreeAgent.ModelFreeAgent):
         logprob = normal_dist.log_prob(action) - tf.math.log(1.0 - tf.pow(squashed_actions, 2) + self.logprob_epsilon)
         logprob = tf.reduce_sum(logprob, axis=-1, keepdims=True)
 
-        return squashed_actions[0], logprob
+        return squashed_actions, logprob
+
+
+    """def actorModel1(self, state):
+        w_bound = 3e-3
+
+        input_shape = self.state_size
+        inputA = Input(input_shape)
+        x = Flatten()(inputA)
+        x = Dense(24, activation='relu')(x)  # fully connected
+        x = Dense(24, activation='relu')(x)
+        mean = Dense(self.action_size, kernel_initializer=tf.random_uniform_initializer(-w_bound, w_bound), bias_initializer=tf.random_uniform_initializer(-w_bound, w_bound))(x)
+        log_std = Dense(self.action_size, kernel_initializer=tf.random_uniform_initializer(-w_bound, w_bound))(x)
+        log_std_clipped = tf.clip_by_value(log_std, -20, 2)
+        normal_dist = tf.compat.v1.distributions.Normal(mean, tf.exp(log_std_clipped))
+        action = tf.stop_gradient(normal_dist.sample())
+        squashed_actions = tf.tanh(action)
+        logprob = normal_dist.log_prob(action) - tf.math.log(1.0 - tf.pow(squashed_actions, 2) + self.logprob_epsilon)
+        logprob = tf.reduce_sum(logprob, axis=-1, keepdims=True)
+
+        return logprob"""
 
     def q_network(self):
         # Generate critic network model
@@ -163,13 +200,14 @@ class SAC(modelFreeAgent.ModelFreeAgent):
        #  """Get the actions for a batch of states."""
        # return self.actor_network(states)[0]
 
-    def choose_action(self, state: np.ndarray):
+    def choose_action(self, state):
         """Get the action for a single state."""
         bg_noise = np.zeros(self.action_size)
         bg_noise = self.ou_noise(bg_noise, dim=self.action_size)
         u, v = self.predict(state, False)
-        sampled_actions = tf.squeeze(u)
-        sampled_actions = sampled_actions.numpy() + bg_noise
+        sampled_actions = u.np.ndarray
+        sampled_actions = np.squeeze(sampled_actions)
+        sampled_actions = sampled_actions + bg_noise
         # Clipping action between bounds -0.3 and 0.3
         legal_action = np.clip(sampled_actions, -0.3, 0.3)[0]
         legal_action = np.squeeze(legal_action)
@@ -200,35 +238,40 @@ class SAC(modelFreeAgent.ModelFreeAgent):
         next_states = next_states.astype(float)
 
         # Computing action and a_tilde
-        action, action_logprob2 = self.actorModel(states)
+        action = self.actorModel()
+        action_logprob2 = self.actor_network(states)
         print(states.shape)
         print(actions.shape)
         print(self.allBatchMask.shape)
 
-        value_target1 = self.softq_target_network(states, actions)
-        value_target2 = self.softq_target_network2(states, actions)
+        input1 = tf.keras.Input(shape=self.state_size, dtype=tf.float64)
+        input2 = tf.keras.Input(shape=self.action_size, dtype=tf.float64)
+
+        value_target1 = self.softq_network([input1, input2])
+        value_target2 = self.softq_network2([input1, input2])
 
         # Taking the minimum of the q-functions values
         next_value_batch = tf.math.minimum(value_target1, value_target2) - self.temperature * action_logprob2
 
         # Computing target for q-functions
         softq_targets = rewards + self.gamma * (1 - dones) * tf.reshape(next_value_batch, [-1])
-        softq_targets = tf.reshape(softq_targets, [self.batch_size, 1])
+        # softq_targets = tf.reshape(softq_targets, [self.batch_size, 1])
 
         # Gradient descent for the first q-function
         with tf.GradientTape() as softq_tape:
-            softq = self.softq_network(states, self.allBatchMask)
+            softq = self.softq_network([input1, input2])
             softq_loss = tf.reduce_mean(tf.square(softq - softq_targets))
 
         # Gradient descent for the second q-function
         with tf.GradientTape() as softq_tape2:
-            softq2 = self.softq_network2(states, self.allBatchMask)
+            softq2 = self.softq_network2([input1, input2])
             softq_loss2 = tf.reduce_mean(tf.square(softq2 - softq_targets))
 
         # Gradient ascent for the policy (actor)
         with tf.GradientTape() as actor_tape:
-            actions, action_logprob = self.actorModel(states)
-            new_softq = tf.math.minimum(self.softq_network(states, actions), self.softq_network2(states, actions))
+            # actions = self.actorModel()
+            actions, action_logprob = self.actor_network(states)
+            new_softq = tf.math.minimum(self.softq_network([input1, input2]), self.softq_network2([input1, input2]))
 
             # Loss implementation from the pseudocode -> works worse
             # actor_loss = tf.reduce_mean(action_logprob - new_softq)
@@ -237,13 +280,15 @@ class SAC(modelFreeAgent.ModelFreeAgent):
             advantage = tf.stop_gradient(action_logprob - new_softq)
             actor_loss = tf.reduce_mean(action_logprob * advantage)
 
+        # print(self.actorModel.layers[0].trainable_weights)
+
         # Computing the gradients with the tapes and applying them
-        actor_gradients = actor_tape.gradient(actor_loss, self.actorModel.trainable_weights)
         softq_gradients = softq_tape.gradient(softq_loss, self.softq_network.trainable_weights)
         softq_gradients2 = softq_tape2.gradient(softq_loss2, self.softq_network2.trainable_weights)
+        actor_gradients = actor_tape.gradient(actor_loss, self.actor_network.trainable_weights)
 
         # Minimize gradients wrt weights
-        self.actor_optimizer.apply_gradients(zip(actor_gradients, self.actorModel.trainable_weights))
+        self.actor_optimizer.apply_gradients(zip(actor_gradients, self.actor_network.trainable_weights))
         self.softq_optimizer.apply_gradients(zip(softq_gradients, self.softq_network.trainable_weights))
         self.softq_optimizer2.apply_gradients(zip(softq_gradients2, self.softq_network2.trainable_weights))
 
@@ -284,22 +329,22 @@ class SAC(modelFreeAgent.ModelFreeAgent):
 
         return states, actions, next_states, rewards, dones
 
-    def predict(self, state: np.ndarray, isTarget):
+    def predict(self, state, isTarget):
 
-        # shape = (-1,) + self.state_size
-        # state = np.reshape(state, shape)
+        shape = (-1,) + self.state_size
+        state = np.reshape(state, shape)
         # state = state(float)
 
         if isTarget:
             print("Target achieved")
             # result = self.q_network([state])
         else:
-            result1, result2 = self.actorModel(state)
+            result1 = self.actor_network(state)
 
-        return result1, result2
+        return result1
 
     def save(self, filename):
-        mem = self.actorModel.get_weights()
+        mem = self.actor_network.get_weights()
         joblib.dump((SAC.displayName, mem), filename)
         print('Model saved')
 
@@ -308,14 +353,14 @@ class SAC(modelFreeAgent.ModelFreeAgent):
         if name != SAC.displayName:
             print('load failed')
         else:
-            self.actorModel.set_weights(mem)
+            self.actor_network.set_weights(mem)
             # self.target.set_weights(mem)
 
     def memsave(self):
-        return self.actorModel.get_weights()
+        return self.actor_network.get_weights()
 
     def memload(self, mem):
-        self.actorModel.set_weights(mem)
+        self.actor_network.set_weights(mem)
         # self.target.set_weights(mem)
 
     def reset(self):
